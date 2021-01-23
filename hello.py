d53da16eaa40ec48d31de86f4fbbffe50786bb65
fuzzyhappy <student.e.wang@gmail.com>
2021-01-23 00:43:11 -0500

    Spent 3 hours on 12 lines why am I so bad

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, request, redirect
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('boilermake-8-project-firebase-adminsdk-n45vg-c326d22181.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+query_ref = db.collection(u'profdata')
 
 app = Flask(__name__)
 
@@ -14,4 +25,5 @@ def default():
 def add_name():
     name = request.form['name']
     NAMES.append({'name' : name})
+    print(query_ref)
     return redirect('/')
