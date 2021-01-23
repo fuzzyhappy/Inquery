728b99c8168c27ceb5be751b8535b1b2cb0ad87d
fuzzyhappy <student.e.wang@gmail.com>
2021-01-23 15:39:26 -0500

    search by similarity

--- hello.py
+++ hello.py
@@ -13,22 +13,17 @@ db = firestore.client()
 query_ref = db.collection(u'profdata')
 
 app = Flask(__name__)
-<<<<<<< HEAD
-    
-=======
-
-testData = { "bob" : {"contact" : "bob@lmao.com", "department" : "cs departmnet", "area" : "memes", "links" : "somelink.com"},
-"yeet" : {"contact" : "xd@lmao.com", "department" : "cs departmnet", "area" : "minecraft", "links" : "morelinks.com"},
-"name1" : {"contact" : "kjl@kjl.com", "department" : "cs departmnet", "area" : "data", "links" : "morelinksssss.com"},
-"babooshka" : {"contact" : "pp@pp.com", "department" : "cs departmnet", "area" : "memes", "links" : "virus.com"},
-"nam2" : {"contact" : "dffds@dsf.com", "department" : "cs departmnet", "area" : "minecraft", "links" : "cancer.gov"}
-}
-def check_sim(word1, word2):
+def check_sim(query, words):
     nlp = spacy.load('en_vectors_web_lg')
-    doc1 = nlp(word1)
-    doc2 = nlp(word2)
-    return doc1.similarity(doc2)
->>>>>>> ad3a98c55a87b0613cc99cdaa47231b75f592a20
+    max_sim = 0
+    doc1 = nlp(query)
+    for word in words:
+        doc2 = nlp(word)
+        sim = doc1.similarity(doc2)
+        if sim > max_sim:
+            max_sim = sim
+    return max_sim
+    
 @app.route('/', methods = ['GET'])
 def default():
     return render_template('index.html')
@@ -38,7 +33,7 @@ def retrieve():
     returnData = {}
     docs = db.collection(u'profdata').stream()
     for doc in docs:
-        if (u'researchArea' in doc.to_dict() and request.form[u'area'], doc.to_dict()[u'researchArea']) >=.70):
+        if (u'researchArea' in doc.to_dict() and check_sim(request.form[u'area'], doc.to_dict()[u'researchArea']) >=.70):
             print(f'{doc.id} => {doc.to_dict()}')
             returnData[doc.id] = doc.to_dict()
     return render_template('index.html', data = returnData)
